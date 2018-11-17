9f1df6f4c40b1a27fcea7f8bfc63d2daf5b77d66
Madsen Phillip <madsen.phillip@dish.com>
2018-11-16 18:11:26 -0700

    updated

--- app/Clip.php
+++ app/Clip.php
@@ -69,18 +69,69 @@ class Clip extends Model implements HasMedia
 			->useDisk('images');
 	}
 
-
-
- 	public function registerMediaConversions(Media $media = null)
+    public function registerMediaConversions(Media $media = null)
     {
-				$this->addMediaConversion('thumbs')
-                    ->useDisk('thumbs')
-                    ->width(150)
-                    ->height(120)
-                    ->performOnCollections('videos', 'images')
-                    ->nonQueued()
-                    ->optimize()
-                    ->withResponsiveImages();
+
+		// $this->addMediaConversion('video')
+  //           ->width(560)//480
+  //           ->height(315)//270
+  //           ->extractVideoFrameAtSecond(2)
+  //           ->extractVideoFrameAtSecond(20)
+  //           ->extractVideoFrameAtSecond(30)
+  //           ->optimize()
+  //           ->nonQueued()
+  //           ->withResponsiveImages()
+  //           ->performOnCollections('videos');
+		$this->addMediaConversion('thumb')
+		     ->width(480)
+		     ->height(270)
+             ->extractVideoFrameAtSeconds(30)
+		     ->performOnCollections('videos');
+
+		// $this->addMediaConversion('twenty')
+		//      ->width(480)
+		//      ->height(270)
+  //            ->extractVideoFrameAtSeconds(20)
+		//      ->performOnCollections('videos');
+
+		// $this->addMediaConversion('two')
+		//      ->width(480)
+		//      ->height(270)
+  //            ->extractVideoFrameAtSeconds(2)
+		//      ->performOnCollections('videos');
+
+        $this
+            ->addMediaConversion('thumbs')
+        	->performOnCollections('images')
+            ->greyscale()
+            ->sharpen(10)
+            ->withResponsiveImages();
+
+        $this->addMediaConversion('old-picture')
+              ->sepia()
+              ->performOnCollections('images')
+              ->border(10, 'black', Manipulations::BORDER_OVERLAY);
     }
+
+ // 	public function registerMediaConversions(Media $media = null)
+ //    {
+ //    	$this->addMediaConversion('videos')
+ //            ->width(560)//480
+ //            ->height(315)//270
+ //            ->extractVideoFrameAtSecond(2)
+ //            ->extractVideoFrameAtSecond(20)
+ //            ->extractVideoFrameAtSecond(30)
+ //            ->nonQueued()
+ //            // ->withResponsiveImages()
+ //            ->performOnCollections('videos');
+
+	// 	$this->addMediaConversion('thumbs')
+ //            // ->useDisk('thumbs')
+ //            ->width(150)
+ //            ->height(120)
+ //            ->performOnCollections('images')
+ //            ->nonQueued()
+ //            ->optimize();
+	// }
     
 }
