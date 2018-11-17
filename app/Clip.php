452215ac62e4c63aa7ed797072051e1171e1b5f5
Madsen Phillip <madsen.phillip@dish.com>
2018-11-16 18:20:36 -0700

    update

--- app/Clip.php
+++ app/Clip.php
@@ -21,43 +21,7 @@ class Clip extends Model implements HasMedia
     protected $fillable = ['videos', 'images'];
     protected $hidden = [];
    
-	// public function registerMediaCollections()
-	// {
-	    // $this->addMediaCollection('videos')
-	    //     ->acceptsFile(function (File $file) {
-	    //         return $file->mimeType === 'video/mp4';
-	    //     })
-	    //     ->useDisk('videos')
-	//         ->registerMediaConversions(function( Media $media = null){
- //                $this->addMediaConversion('video')
- //                    ->width(560)//480
- //                    ->height(315)//270
- //                    ->extractVideoFrameAtSecond(2)
- //                    ->extractVideoFrameAtSecond(20)
- //                    ->extractVideoFrameAtSecond(30)
- //                    // ->optimize()
- //                    ->nonQueued()
- //                    ->withResponsiveImages()
- //                    ->performOnCollections('videos');
- //            });
-
- //        $this->addMediaCollection('images')
- //            ->acceptsFile(function (File $file) {
-	//             return $file->mimeType === 'image/jpeg';
-	//         })
-	//         ->useDisk('images')
-	//         ->registerMediaConversions(function ( Media $media = null ) {
- //                $this->addMediaConversion('thumb')
- //                    ->useDisk('thumbs')
- //                    ->width(150)
- //                    ->height(120)
- //                    ->performOnCollections('videos', 'images')
- //                    ->nonQueued()
- //                    ->optimize()
- //                    ->withResponsiveImages();
- //            });
-	// }
-
+ 
 	public function registerMediaCollections()
 	{
 		$this->addMediaCollection('videos')
@@ -71,34 +35,13 @@ class Clip extends Model implements HasMedia
 
     public function registerMediaConversions(Media $media = null)
     {
+ 
 
-		// $this->addMediaConversion('video')
-  //           ->width(560)//480
-  //           ->height(315)//270
-  //           ->extractVideoFrameAtSecond(2)
-  //           ->extractVideoFrameAtSecond(20)
-  //           ->extractVideoFrameAtSecond(30)
-  //           ->optimize()
-  //           ->nonQueued()
-  //           ->withResponsiveImages()
-  //           ->performOnCollections('videos');
-		$this->addMediaConversion('thumb')
-		     ->width(480)
-		     ->height(270)
-             ->extractVideoFrameAtSeconds(30)
+		$this->addMediaConversion('screenshot')
+		     ->width(368)
+		     ->height(232)
+		     ->extractVideoFrameAtSecond(20)
 		     ->performOnCollections('videos');
-
-		// $this->addMediaConversion('twenty')
-		//      ->width(480)
-		//      ->height(270)
-  //            ->extractVideoFrameAtSeconds(20)
-		//      ->performOnCollections('videos');
-
-		// $this->addMediaConversion('two')
-		//      ->width(480)
-		//      ->height(270)
-  //            ->extractVideoFrameAtSeconds(2)
-		//      ->performOnCollections('videos');
 
         $this
             ->addMediaConversion('thumbs')
@@ -112,26 +55,5 @@ class Clip extends Model implements HasMedia
               ->performOnCollections('images')
               ->border(10, 'black', Manipulations::BORDER_OVERLAY);
     }
-
- // 	public function registerMediaConversions(Media $media = null)
- //    {
- //    	$this->addMediaConversion('videos')
- //            ->width(560)//480
- //            ->height(315)//270
- //            ->extractVideoFrameAtSecond(2)
- //            ->extractVideoFrameAtSecond(20)
- //            ->extractVideoFrameAtSecond(30)
- //            ->nonQueued()
- //            // ->withResponsiveImages()
- //            ->performOnCollections('videos');
-
-	// 	$this->addMediaConversion('thumbs')
- //            // ->useDisk('thumbs')
- //            ->width(150)
- //            ->height(120)
- //            ->performOnCollections('images')
- //            ->nonQueued()
- //            ->optimize();
-	// }
-    
+ 
 }
